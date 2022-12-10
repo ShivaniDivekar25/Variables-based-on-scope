b307f2d68b8354409ba605700d79bc612e2672c6
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-10 13:09:11 +0530

    Added Local variable concept

---
+++ Variable types/Constructor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_types
+{
+    internal class Constructor
+    {
+        //Default constructor
+        public Constructor()
+        {
+            Console.WriteLine("Default constructor");
+        }
+
+        // Paramitrized constructor
+        public Constructor(int phone)
+        {
+            Console.WriteLine("Parameterized constructor");
+        }
+
+
+        // Local variable - Scope is only to the method
+        public static void StudentAge(int phoneNumber)
+        {
+            int age = 0;
+            age = age + 10;
+            Console.WriteLine("Student age is {0} and phoneNumber is {1}: ", age, phoneNumber);
+        }
+    }
+}
---
+++ Variable types/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Variable_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Constructor.StudentAge(1234567895);
+            Constructor constructor1 = new Constructor();
+            Constructor constructor = new Constructor(456);
+            Console.ReadLine();
+
+        }
+    }
+}
