45a1ea3a67357626831353c8ac06eb82404d6c1b
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-13 17:45:04 +0530

    Adding Instance variable program

---
+++ Variable-Instance1/Instance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_Instance1
+{
+    internal class Instance
+    {
+        public int engMarks;
+        public int mathsMarks;
+        public int phyMarks;
+
+        public void MarksDetails()
+        {
+            Console.WriteLine("English:{0} Maths:{1} Physics:{2}", engMarks, mathsMarks, phyMarks);
+        }
+    }
+}
---
+++ Variable-Instance1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Variable_Instance1
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Instance marks = new Instance();
+            marks.engMarks = 90;
+            marks.mathsMarks = 80;
+            marks.phyMarks = 93;
+            marks.MarksDetails();
+
+            Instance marks1 = new Instance();
+            marks1.engMarks = 95;
+            marks1.mathsMarks = 70;
+            marks1.phyMarks = 97;
+            marks1.MarksDetails();
+        }
+    }
+}
